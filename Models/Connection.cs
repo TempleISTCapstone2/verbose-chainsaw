f9911cd12f0dabb44351645d5afc4c0e66dc19ea
Fabrizio <tue82272@temple.edu>
2022-05-05 13:30:30 -0400

    Initial commit

---
+++ Models/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeScanning.Models
+{
+    public class Connection
+    {
+        private string username = "username";
+        private string password = "H3ll0";
+    }
+}
